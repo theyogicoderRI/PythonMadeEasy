816b74b007ec750e4b7c39e8d7b53a3f53c3e528
YogiCoderRI <46927195+theyogicoderRI@users.noreply.github.com>
2019-01-22 09:07:42 -0500

    Add files via upload

---
+++ 1-Intro To Python/idle-code-examples/idle-code-examples.py
@@ -0,0 +1,24 @@
+# calculate the total area of a room
+
+'''Measure the length and the width of a square or rectangular room. Use the standard
+2(Length + Width) = Perimeter formula to find the perimeter of a room
+'''
+
+##length = 10
+##width = 17
+##print(2*(length+width))
+
+
+'''calculate the total restuarant bills using a 10% tip, 15% tip and a 20% tip
+The total bill is $120'''
+
+total = 120
+ten, fifteen, twenty = .10, .15, .20
+
+print("10% total is: ", (total*ten)+ total)
+print("15% total is: ", (total*fifteen)+ total)
+print("20% total is: ", (total*twenty)+ total)
+
+
+
+
