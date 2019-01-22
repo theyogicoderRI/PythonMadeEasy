c9651d16b9a85ca1419b203d68b367de43a4ccab
YogiCoderRI <46927195+theyogicoderRI@users.noreply.github.com>
2019-01-22 09:40:40 -0500

    Add files via upload

---
+++ 4- User Input/user-in.py
@@ -0,0 +1,23 @@
+# get input from a user
+##
+##user_input = input('Please Enter a Word or Two: ')
+##print("You Entered: ", user_input)
+
+
+                   
+
+print("I am a teller of furture professions.")
+print("My name is Madam Py-Ton")
+print("I am happy to tell you your profession from you answering a few basic questions.")
+get_color = input("1. Please Provide Your Favorite Color? ")
+get_number = int(input("2. What is Your Favorite number? "))
+
+##print("*" * 80)
+##print("Your Favorite Color is: ", get_color)
+##print("Your Favorite Number  is: " , get_number)
+##print("You will soon be an expert Python Coder!")
+##print(get_color, get_number)
+##
+##print("Your Favorite Number  is: " +  get_number)
+print("Your Favorite Number and Color  is: " +  str(get_number) + " " +  get_color)
+
