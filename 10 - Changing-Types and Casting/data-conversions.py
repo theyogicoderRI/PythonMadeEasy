1e2f07726544df5f68e4cc279f72de76d70795be
YogiCoderRI <46927195+theyogicoderRI@users.noreply.github.com>
2019-01-22 10:01:08 -0500

    Add files via upload

---
+++ 10 - Changing-Types and Casting/data-conversions.py
@@ -0,0 +1,31 @@
+##my_int = 100  # data type is an int
+##my_float = 100.5 # data type is a float
+##
+##my_total = (my_int + my_float) # data type is a float
+##
+##print(my_total)
+##print(type(my_int))
+##print(type(my_total))
+
+##
+##
+##user_int = input("Please enter the total bill amount: ")
+##print("user_int type is: ", type(user_int))
+##
+##tip = .15
+##total = (tip * float(user_int) + float(user_int))
+##print("the total bill is: ", total)
+
+
+##list1 = [1,1,2,3,4,4,5,5,6,7, 7]
+##
+### to remove duplicates
+### convert list1 to a set
+##
+##remove_dups = set(list1)
+##print(remove_dups)
+
+
+shoes = 125
+
+print("Your shoes cost: " + "$" + str(shoes))
